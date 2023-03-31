27b0ff667053b346cc62dc31c8637f430ff18fa4
Matheus <matheus.oliveira92@hotmail.com>
2023-03-31 17:37:45 -0300

    agora com vpc e subnet corretos

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -11,9 +11,9 @@ variable "key_pair_name" {
 }
 
 variable "airflow_subnet_id" {
-  default = "subnet-4cef5427"
+  default = "subnet-06f154f064f6a2123"
 }
 
 variable "vpc_id" {
-  default = "vpc-0effce9e11653d215"
+  default = "vpc-02016ebd88efc1260"
 }
